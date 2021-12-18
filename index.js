18b023205279b603e4b1c41778573eb695b19fbe
Guru <guru@mobil80.com>
2021-12-18 12:26:08 +0530

    payment changed to usd

--- index.js
+++ index.js
@@ -5,12 +5,33 @@ const env = require('dotenv').config({path: '.env'});
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 app.get('/', async (req, res) => {
-    try {
+    try {   
+        await stripe.customers.create({
+            name: 'Guru',
+            address: {
+                line1: '599 HMT Layout',
+                postal_code: '560097',
+                city: 'Bangalore',
+                state: 'Karnataka',
+                country: 'India',
+            },
+        });
+
         await stripe.paymentIntents.create({
-            amount: 20,
-            currency: "inr",
+            amount: 200,
+            currency: "usd",
             description: "JEXtream Account Renewal",
             payment_method_types: ['card'],
+            shipping: {
+                name: 'Guru',
+                address: {
+                  line1: '599 HMT Layout',
+                  postal_code: '560097',
+                  city: 'Bangalore',
+                  state: 'Karnataka',
+                  country: 'India',
+                },
+              },
         }).then((result) => {
             console.log(result);
             res.send({client_secret: result.client_secret, err: null});
